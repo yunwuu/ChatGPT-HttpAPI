8e5c4bf4556a8c6ee8b2e4738d488e1b829a8662
Cocogoat <123147018+cocogoat-moe@users.noreply.github.com>
2023-02-18 14:50:20 +0000

    Update

--- app.py
+++ app.py
@@ -12,8 +12,14 @@ if(os.getenv("EMAIL") is None or os.getenv("PASSWORD") is None):
     exit(1)
 
 
-@app.route("/chatgpt/", methods=["POST"])
+@app.route("/chatgpt/", methods=["POST", "OPTIONS"])
 def chat():
+    if request.method == "OPTIONS":
+        resp = Response()
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        resp.headers['Access-Control-Allow-Methods'] = 'POST'
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return resp
     data = request.get_json()
     if data.get("text") is None:
         err_resp = Response("Please provide a text to chat with")
